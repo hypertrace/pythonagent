556b330c6983efa0579ce224167dac340d7b9e3e
rcbjBlueMars <broeckel@rcbj.net>
2021-03-11 21:00:33 -0800

    Updates to grpc instrumentation wrapper. Not working yet.

--- instrumentation/grpc/__init__.py
+++ instrumentation/grpc/__init__.py
@@ -2,8 +2,12 @@ import sys
 import os.path
 import logging
 import inspect
-import flask;
-from opentelemetry.instrumentation.grpc import GrpcInstrumentorServer, GrpcInstrumentorClient
+import flask
+import grpc
+from contextlib import contextmanager
+from opentelemetry import trace
+from opentelemetry.instrumentation.grpc import GrpcInstrumentorServer, GrpcInstrumentorClient, _server, _client
+from opentelemetry.instrumentation.grpc.version import __version__
 sys.path.append(
     os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 from instrumentation import BaseInstrumentorWrapper
@@ -29,8 +33,29 @@ class GrpcInstrumentorServerWrapper(GrpcInstrumentorServer, BaseInstrumentorWrap
     super().instrument()
 
   def uninstrument(self):
-    logger.debug('Entering GrpcInstrumentorServerWrapper.uninstrument()');
+    logger.debug('Entering GrpcInstrumentorServerWrapper.uninstrument()')
     super().uninstrument()
+
+  def _instrument(self, **kwargs):
+    logger.debug('Entering GrpcInstrumentorServerWrapper._instument().')
+#    super()._instrument(**kwargs)
+    self._original_wrapper_func = grpc.server
+
+    def server(*args, **kwargs):
+      logger.debug('Entering wrapper interceptors set')
+      if "interceptors" in kwargs:
+        # add our interceptor as the first
+        logger.debug('Setting server_interceptor_wrapper() as interceptor')
+        kwargs["interceptors"].insert(1, server_interceptor_wrapper())
+      else:
+        kwargs["interceptors"] = [server_interceptor_wrapper()]
+      return self._original_wrapper_func(*args, **kwargs)
+
+    grpc.server = server
+    
+  def _uninstrument(self, **kwargs):
+    logger.debug('Entering GrpcInstrumentorServerWrapper._uninstrument()')
+    super()._uninstrument(**kwargs)
 
 class GrpcInstrumentorClientWrapper(GrpcInstrumentorClient, BaseInstrumentorWrapper):
   def __init__(self):
@@ -44,3 +69,39 @@ class GrpcInstrumentorClientWrapper(GrpcInstrumentorClient, BaseInstrumentorWrap
   def uninstrument(self):
     logger.debug('Entering GrpcInstrumentorClientWrapper.uninstrument().')
     super()._uninstrument()
+
+def client_interceptor_wrapper(tracer_provider=None):
+  from . import _client
+  tracer = trace.get_tracer(__name__, __version__, tracer_provider)
+  return OpenTelemetryClientInterceptor(tracer)
+
+def server_interceptor_wrapper(tracer_provider=None):
+  from . import _server
+  tracer = trace.get_tracer(__name__, __version__, tracer_provider)
+  return OpenTelemetryServerInterceptorWrapper(tracer)
+
+class OpenTelemetryServerInterceptorWrapper(_server.OpenTelemetryServerInterceptor):
+  def __init__(self, tracer):
+    logger.debug('Entering OpenTelemetryServerInterceptorWrapper.__init__().')
+    super().__init__(tracer)
+
+#  @contextmanager
+  def _set_remote_context(self, servicer_context):
+    logger.debug('Entering OpenTelemetryServerInterceptorWrapper._set_remote_context().')
+    super()._set_remote_context(servicer_context)
+
+  def _start_span(self, handler_call_details, context):
+    logger.debug('Entering OpenTelemetryServerInterceptorWrapper._start_span().')
+    super()._start_span(handler_call_details, context)
+
+  def intercept_service(self, continuation, handler_call_details):
+    logger.debug('Entering OpenTelemetryServerInterceptorWrapper.intercept_service().')
+    super().intercept_service(continuation, handler_call_details)
+
+  def _intercept_server_stream(self, behavior, handler_call_details, request_or_iterator, context):
+    logger.debug('Entering OpenTelemetryServerInterceptorWrapper.intercept_server_stream().')
+    super()._intercept_server_stream(behavior, handler_call_details, request_or_iterator, context)
+
+class OpenTelemetryClientInterceptorWrapper(_client.OpenTelemetryClientInterceptor):
+  def __init(self, tracer):
+    super().__init__(tracer)
