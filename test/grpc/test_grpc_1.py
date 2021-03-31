c022514fce12bda6bae7910743da1b69049d7e9a
nitin <nsahai03@gmail.com>
2021-03-31 00:04:00 -0700

    added new test cases

--- test/grpc/test_grpc_1.py
+++ test/grpc/test_grpc_1.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 from concurrent import futures
+import re
 import sys
 import os
 import logging
@@ -121,7 +122,9 @@ def exit_callback():
 #        "rpc.response.body": "message: \"Hello, you!\"\n"
       assert flaskSpanAsObject['attributes']['rpc.system'] == 'grpc'
       assert flaskSpanAsObject['attributes']['rpc.method'] == 'SayHello'
-      assert flaskSpanAsObject['attributes']['rpc.request.metadata.user-agent'] == 'grpc-python/1.36.1 grpc-c/15.0.0 (linux; chttp2)'
+      user_agent_re = re.compile('grpc-python/1.36.1 grpc-c/15.0.0 (.*; chttp2)')
+      assert re.match(user_agent_re, flaskSpanAsObject['attributes']['rpc.request.metadata.user-agent'])
+    #  assert flaskSpanAsObject['attributes']['rpc.request.metadata.user-agent'] == 'grpc-python/1.36.1 grpc-c/15.0.0 (linux; chttp2)'
       assert flaskSpanAsObject['attributes']['rpc.request.body'] == 'name: \"you\"\n'
       assert flaskSpanAsObject['attributes']['rpc.grpc.status_code'] == 0
       assert flaskSpanAsObject['attributes']['rpc.response.metadata.tester2'] == 'tester2'
