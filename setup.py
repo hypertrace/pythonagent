dc0396408f3d260b51bbdb101d229216b0a75b0f
rcbjBlueMars <broeckel@rcbj.net>
2021-04-18 19:59:50 -0700

    setup.py clean so build succeeds.

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-import setuptools
+#import setuptools
+from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-    name="hypertrace-pythonagent",
+setup(
+    name="hypertrace",
     version="0.1.0",
     author="Robert C. Broeckelmann Jr.",
     author_email="robert@iyasec.io",
@@ -15,12 +16,12 @@ setuptools.setup(
     project_urls={
         "Bug Tracker":"https://github.com/Traceableai/pythonagent/issues",
     },
-    classifiers={
+    classifiers=[
         "Programming Language :: Python :: 3",
         "License :: Apache",
         "Operating System :: OS Independent"
-    },
+    ],
     package_dir={"": "src"},
-    packages='setuptools.find_packages(where="hypertrace")',
+    packages=find_packages(where="src"),
     python_requires=">=3.7"
 )
