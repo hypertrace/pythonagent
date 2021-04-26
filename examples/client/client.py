b19d0ba22d66e05a55180eb61341597ec1513509
José Carlos Chávez <jcchavezs@gmail.com>
2021-04-26 13:10:06 +0200

    chore(stream): adds logs to stream processing.

--- examples/client/client.py
+++ examples/client/client.py
@@ -2,15 +2,25 @@ import aiohttp
 import asyncio
 from hypertrace.agent import Agent
 import os
+import json
+import logging
 
 agent = Agent()
 agent.register_aiohttp_client()
 
+logging.basicConfig()
+logging.getLogger().setLevel(logging.DEBUG)
+
 
 async def main():
     async with aiohttp.ClientSession() as session:
-        query = ""
+        isStream = False
         if 'STREAM' in os.environ and os.environ['STREAM'] == 'true':
+            isStream = True
+
+        query = ""
+        if isStream:
+            logging.debug('Receving response stream')
             query = "?stream=true"
 
         async with session.post(
@@ -18,7 +28,17 @@ async def main():
                 data='{"name":"Dave"}',
                 headers={'content-type': 'application/json'}
         ) as resp:
-            resBody = await resp.json()
+            if isStream:
+                # Read the response as a stream
+                content = b''
+                async for line in resp.content:
+                    content += line
+                    logging.debug("Receiving response chunk")
+                resBody = json.loads(content)
+            else:
+                # Read the entire response
+                resBody = await resp.json()
+
             print(resBody['message'])
 
 loop = asyncio.get_event_loop()
--- examples/server/server.py
+++ examples/server/server.py
@@ -2,12 +2,22 @@ from flask import Flask, request, jsonify, make_response, Response
 from hypertrace.agent import Agent
 import time
 import json
+import logging
+
+
+logging.basicConfig()
+logging.getLogger().setLevel(logging.DEBUG)
 
 
 def stream_body(resBody):
+    newLineChars = ['{', '}', ',', '[', ']']
     for c in resBody:
-        time.sleep(0.1)
-        yield c
+        time.sleep(0.3)
+        logging.debug("Sending response chunk")
+        if c in newLineChars:
+            yield c + "\n"
+        else:
+            yield c
 
 
 def create_app():
@@ -19,12 +29,14 @@ def create_app():
     def hello():
         request_data = request.get_json()
         name = request_data['name']
-        resBody = {'message': f'Hello {name}'}
+        resBody = {'id': 1, 'message': f'Hello {name}'}
 
         stream = request.args.get('stream')
         if stream == 'true':
+            # Send the response as stream
             return Response(stream_body(json.dumps(resBody)), mimetype='application/json')
         else:
+            # Send the entire response
             response = make_response(jsonify(resBody))
             response.headers['Content-Type'] = 'application/json'
             return response
