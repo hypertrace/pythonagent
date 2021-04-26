5944bff48a4da41f6b5897130c07fc1cb4257c00
José Carlos Chávez <jcchavezs@gmail.com>
2021-04-26 12:28:43 +0200

    feat(stream): adds support to exercise stream in client/server.

--- examples/client/client.py
+++ examples/client/client.py
@@ -1,19 +1,25 @@
 import aiohttp
 import asyncio
 from hypertrace.agent import Agent
+import os
 
 agent = Agent()
 agent.register_aiohttp_client()
 
+
 async def main():
     async with aiohttp.ClientSession() as session:
+        query = ""
+        if 'STREAM' in os.environ and os.environ['STREAM'] == 'true':
+            query = "?stream=true"
+
         async with session.post(
-                'http://localhost:9000',
+                'http://localhost:9000/' + query,
                 data='{"name":"Dave"}',
                 headers={'content-type': 'application/json'}
         ) as resp:
-            print(await resp.json())
-            #print("Call succeeded: %s" % resBody['message'])
+            resBody = await resp.json()
+            print(resBody['message'])
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
--- examples/server/server.py
+++ examples/server/server.py
@@ -1,5 +1,14 @@
-from flask import Flask, request, jsonify, make_response
+from flask import Flask, request, jsonify, make_response, Response
 from hypertrace.agent import Agent
+import time
+import json
+
+
+def stream_body(resBody):
+    for c in resBody:
+        time.sleep(0.1)
+        yield c
+
 
 def create_app():
     app = Flask(__name__)
@@ -10,7 +19,13 @@ def create_app():
     def hello():
         request_data = request.get_json()
         name = request_data['name']
-        response = make_response(jsonify(message=f'Hello {name}'))
-        response.headers["Content-Type"] = "application/json"
-        return response
+        resBody = {'message': f'Hello {name}'}
+
+        stream = request.args.get('stream')
+        if stream == 'true':
+            return Response(stream_body(json.dumps(resBody)), mimetype='application/json')
+        else:
+            response = make_response(jsonify(resBody))
+            response.headers['Content-Type'] = 'application/json'
+            return response
     return app
