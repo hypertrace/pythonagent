4140008cc0843681c414760fa13388c2650743e0
nitin <nsahai03@gmail.com>
2021-04-28 23:49:01 -0700

    fixed testcase

--- src/hypertrace/agent/constants.py
+++ src/hypertrace/agent/constants.py
@@ -1,5 +1,6 @@
 '''Agent generic exception message'''
 from autoinstrumentation.src.hypertrace.instrumentation.version import __version__
+
 EXCEPTION_MESSAGE = 'Failed to initialize %s instrumentation wrapper: exception=%s, stacktrace=%s'
 INST_WRAP_EXCEPTION_MSSG = 'Failed to initialize %s instrumentation wrapper: exception=%s, stacktrace=%s' # pylint: disable=C0301
 INST_RUNTIME_EXCEPTION_MSSG = 'An error occurred in %s: exception=%s, stacktrace=%s'
