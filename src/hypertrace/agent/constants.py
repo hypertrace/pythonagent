3a192dbef290ad47e05ad26eca242b01b0c7cd62
rcbjBlueMars <broeckel@rcbj.net>
2021-04-29 19:11:33 -0500

    Removing autoinstrumentation dependency

--- src/hypertrace/agent/constants.py
+++ src/hypertrace/agent/constants.py
@@ -1,9 +1,7 @@
-'''Agent generic exception message'''
-from autoinstrumentation.src.hypertrace.instrumentation.version import __version__
-
+'''constants'''
 EXCEPTION_MESSAGE = 'Failed to initialize %s instrumentation wrapper: exception=%s, stacktrace=%s'
 INST_WRAP_EXCEPTION_MSSG = 'Failed to initialize %s instrumentation wrapper: exception=%s, stacktrace=%s' # pylint: disable=C0301
 INST_RUNTIME_EXCEPTION_MSSG = 'An error occurred in %s: exception=%s, stacktrace=%s'
-TELEMETRY_SDK_VERSION = __version__
+TELEMETRY_SDK_VERSION = '0.1.0'
 TELEMETRY_SDK_NAME = 'hypertrace'
 TELEMETRY_SDK_LANGUAGE = 'python'
