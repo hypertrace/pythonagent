6ceccabb161f90d3ee284c8afe24b824d6229266
José Carlos Chávez <jcchavezs@gmail.com>
2021-05-02 22:39:49 +0200

    chore(examples): adds MySQL support in the server example.

---
+++ examples/server/mysql/init.sql
@@ -0,0 +1,4 @@
+CREATE TABLE `users`(
+   id INT AUTO_INCREMENT primary key NOT NULL,
+   `name` VARCHAR(100) NOT NULL
+);
--- examples/server/server.py
+++ examples/server/server.py
@@ -3,13 +3,14 @@ from hypertrace.agent import Agent
 import time
 import json
 import logging
+import mysql.connector
 
 
 logging.basicConfig()
 logging.getLogger().setLevel(logging.DEBUG)
 
 
-def stream_body(resBody):
+def stream_body(resBody: str):
     newLineChars = ['{', '}', ',', '[', ']']
     for c in resBody:
         time.sleep(0.3)
@@ -20,24 +21,42 @@ def stream_body(resBody):
             yield c
 
 
+def insert_user(name: str) -> int:
+    cnx = mysql.connector.connect(database='test',
+                                  username='root',
+                                  password='root',
+                                  host='localhost',
+                                  port=3306)
+    cursor = cnx.cursor()
+    query = "INSERT INTO users (`name`) VALUES (%s)"
+    cursor.execute(query, (name,))
+    id = cursor.lastrowid
+    cnx.close()
+    return id
+
+
 def create_app():
     app = Flask(__name__)
     agent = Agent()
     agent.register_flask_app(app)
+    agent.register_mysql()
 
     @app.route('/', methods=['POST'])
     def hello():
         request_data = request.get_json()
         name = request_data['name']
-        resBody = {'id': 1, 'message': f'Hello {name}'}
+
+        user_id = insert_user(name)
+
+        res_body = {'id': user_id, 'message': f'Hello {name}'}
 
         stream = request.args.get('stream')
         if stream == 'true':
             # Send the response as stream
-            return Response(stream_body(json.dumps(resBody)), mimetype='application/json')
+            return Response(stream_body(json.dumps(res_body)), mimetype='application/json')
         else:
             # Send the entire response
-            response = make_response(jsonify(resBody))
+            response = make_response(jsonify(res_body))
             response.headers['Content-Type'] = 'application/json'
             return response
     return app
