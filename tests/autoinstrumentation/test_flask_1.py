651b8eb77e7a9ce352583445c0de110d594405d0
prodion23 <donniepropst@donnies-mbp.lan>
2021-07-22 16:39:28 -0400

    revert flask test changes, merge latest

--- tests/autoinstrumentation/test_flask_1.py
+++ tests/autoinstrumentation/test_flask_1.py
@@ -1,3 +1,4 @@
+import sys
 import os
 import logging
 import flask
@@ -15,7 +16,6 @@ from opentelemetry import trace as trace_api
 from opentelemetry.sdk.trace import TracerProvider, export
 from opentelemetry.sdk.trace.export.in_memory_span_exporter import InMemorySpanExporter
 from opentelemetry.sdk.trace.export import BatchSpanProcessor, SimpleSpanProcessor
-from hypertrace.agent import Agent
 
 def setup_custom_logger(name):
   try:
@@ -50,11 +50,6 @@ class FlaskServer(threading.Thread):
     self.srv.serve_forever()
     self.start()
 
-  def shutdown(self):
-    logger.info('Shutting down server.')
-    self.srv.shutdown()
-
-#@pytest.mark.serial
 def test_run():
   logger = setup_custom_logger(__name__)
   logger.info('Initializing flask app.')
@@ -64,15 +59,6 @@ def test_run():
   def before_first_request():
     logger.debug("test_program: before_first_request() called")
 
-  @app.before_request
-  def before_request():
-      logger.debug("test_progam: before_request() called")
-
-  @app.after_request
-  def after_request(response):
-      logger.debug("test_program: after_request() called")
-      return response
-
   @app.route("/route1")
   def testAPI1():
     logger.info('Serving request for /route1.')
@@ -81,110 +67,30 @@ def test_run():
     response.data = str('{ "a": "a", "xyz": "xyz" }')
     return response
 
-  @app.route("/route2")
-  def testAPI2():
-    logger.info('Serving request for /route2.')
-    response = flask.Response(mimetype='application/json')
-    response.headers['tester3'] = 'tester3'
-    response.data = str('{ "a": "a", "xyz": "xyz" }')
-    return response
-
-  @app.route("/terminate")
-  def terminate():
-    logger.info('Serving request for /terminatae.')
-    shutdown_server()
-    response = flask.Response()
-    response.data = { 'a': 'a', 'xyz': 'xyz' }
-    return response
-
   logger.info('Flask app initialized.')
-
-  #
-  # Code snippet here represents the current initialization logic
-  #
-  logger.info('Initializing agent.')
-  agent = Agent()
-  agent.register_flask_app(app)
-  #
-  # End initialization logic for Python Agent
-  #
-  logger.info('Agent initialized.')
 
   server = FlaskServer(app)
 
-  # Setup In-Memory Span Exporter
-  logger.info('Agent initialized.')
-  logger.info('Adding in-memory span exporter.')
-  memoryExporter = InMemorySpanExporter()
-  simpleExportSpanProcessor = SimpleSpanProcessor(memoryExporter)
-  agent.register_processor(simpleExportSpanProcessor)
-  logger.info('Added in-memoy span exporter')
-
-#  # Setup Jaeger Exporter
-#  logger.info('Adding jaeger span exporter.')
-#  jaegerExporter = JaegerExporter(
-#      agent_host_name='localhost',
-#      agent_port=6831,
-#  )
-#  batchExportSpanProcessor = BatchSpanProcessor(jaegerExporter)
-#  agent.register_processor(batchExportSpanProcessor)
-#  logger.info('Added jaeger span exporter.')
-
   logger.info('Running test calls.')
   with app.test_client() as c:
-    try:
+    try: 
       logger.info('Making test call to /route1')
-      r1 = app.test_client().get('http://localhost:5000/route1', headers={ 'tester1': 'tester1', 'tester2':'tester2'})
-      # Get all of the in memory spans that were recorded for this iteration
-      span_list = memoryExporter.get_finished_spans()
-      # Confirm something was returned.
-      assert span_list
-      # Confirm there are three spans
-      logger.debug('len(span_list): ' + str(len(span_list)))
-      assert len(span_list) == 1
-      logger.debug('span_list: ' + str(span_list[0].attributes))
-      flaskSpanAsObject = json.loads(span_list[0].to_json())
-      # Check that the expected results are in the flask extended span attributes
-      assert flaskSpanAsObject['attributes']['http.method'] == 'GET'
-      assert flaskSpanAsObject['attributes']['http.target'] == '/route1'
-      assert flaskSpanAsObject['attributes']['http.request.header.tester1'] == 'tester1'
-      assert flaskSpanAsObject['attributes']['http.request.header.tester2'] == 'tester2'
-      assert flaskSpanAsObject['attributes']['http.response.header.content-type'] == 'application/json'
-      assert flaskSpanAsObject['attributes']['http.response.body'] == '{ "a": "a", "xyz": "xyz" }'
-      assert flaskSpanAsObject['attributes']['http.status_code'] == 200
-      assert flaskSpanAsObject['attributes']['http.response.header.tester3'] == 'tester3'
-      memoryExporter.clear()
-      logger.info('Making test call to /route2')
-      r2 = app.test_client().get('http://localhost:5000/route2', headers={ 'tester1': 'tester1', 'tester2':'tester2'})
-      # Confirm something was returned.
-      assert span_list
-      # Confirm there are three spans
-      logger.debug('len(span_list): ' + str(len(span_list)))
-      assert len(span_list) == 1
-      logger.debug('span_list: ' + str(span_list[0].attributes))
-      flaskSpanAsObject = json.loads(span_list[0].to_json())
-      # Check that the expected results are in the flask extended span attributes
-      assert flaskSpanAsObject['attributes']['http.method'] == 'GET'
-      assert flaskSpanAsObject['attributes']['http.target'] == '/route1'
-      assert flaskSpanAsObject['attributes']['http.request.header.tester1'] == 'tester1'
-      assert flaskSpanAsObject['attributes']['http.request.header.tester2'] == 'tester2'
-      assert flaskSpanAsObject['attributes']['http.response.header.content-type'] == 'application/json'
-      assert flaskSpanAsObject['attributes']['http.response.body'] == '{ "a": "a", "xyz": "xyz" }'
-      assert flaskSpanAsObject['attributes']['http.status_code'] == 200
-      assert flaskSpanAsObject['attributes']['http.response.header.tester3'] == 'tester3'
-      memoryExporter.clear()
+      r1 = app.test_client().get(
+          'http://localhost:5000/route1',
+          headers={ 'tester1': 'tester1',
+                    'tester2':'tester2'
+                  }
+      )
       logger.info('Reading /route1 response.')
       a1 = r1.get_json()['a']
-      logger.info('Reading /route2 response.')
-      a2 = r2.get_json()['a']
       assert a1 == 'a'
-      assert a2 == 'a'
       logger.info('r1 result: ' + str(a1))
-      logger.info('r2 result: ' + str(a2))
-      logger.info('Exiting from flask instrumentation test.')
       return 0
     except:
       logger.error('Failed to initialize flask instrumentation wrapper: exception=%s, stacktrace=%s',
         sys.exc_info()[0],
         traceback.format_exc())
       raise sys.exc_info()[0]
+
+if __name__ == '__main__':
+    test_run()
