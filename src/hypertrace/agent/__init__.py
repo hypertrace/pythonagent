afcf7a5e588d16f6e270b656bdc3851a161d00b7
prodion23 <donniepropst@donnies-mbp.lan>
2021-08-11 13:01:48 -0400

    lint

--- src/hypertrace/agent/__init__.py
+++ src/hypertrace/agent/__init__.py
@@ -30,7 +30,7 @@ class Agent:
         if cls._instance is None:
             with cls._singleton_lock:
                 logger.debug('Creating Agent')
-                logger.debug(f'Python version: {sys.version}')
+                logger.debug('Python version: %s', sys.version)
                 cls._instance = super(Agent, cls).__new__(cls)
                 cls._instance._initialized = False
         else:
