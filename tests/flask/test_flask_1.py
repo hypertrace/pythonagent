b2bf199f24b3c40f8e88a44e3c576cff2c45424f
Donnie <donniepropst@gmail.com>
2022-01-10 14:51:49 -0500

    Dont str.replace body content + add test (#259)
    * Dont body replace + add test
    
    * update test
    
    Co-authored-by: prodion23 <donniepropst@donnies-mbp.lan>

--- tests/flask/test_flask_1.py
+++ tests/flask/test_flask_1.py
@@ -79,7 +79,8 @@ def test_run():
     logger.info('Serving request for /route2.')
     response = flask.Response(mimetype='application/json')
     response.headers['tester3'] = 'tester3'
-    response.data = str('{ "a": "a", "xyz": "xyz" }')
+    json_data = {"message": "Hello [('mysql.infoschema', 'localhost')]"}
+    response.data = json.dumps(json_data)
     return response
 
   @app.route("/terminate")
@@ -111,17 +112,8 @@ def test_run():
   memoryExporter = InMemorySpanExporter()
   simpleExportSpanProcessor = SimpleSpanProcessor(memoryExporter)
   agent.register_processor(simpleExportSpanProcessor)
-  logger.info('Added in-memoy span exporter')
+  logger.info('Added in-memory span exporter')
 
-#  # Setup Jaeger Exporter
-#  logger.info('Adding jaeger span exporter.')
-#  jaegerExporter = JaegerExporter(
-#      agent_host_name='localhost',
-#      agent_port=6831,
-#  )
-#  batchExportSpanProcessor = BatchSpanProcessor(jaegerExporter)
-#  agent.register_processor(batchExportSpanProcessor)
-#  logger.info('Added jaeger span exporter.')
 
   logger.info('Running test calls.')
   with app.test_client() as c:
@@ -150,6 +142,7 @@ def test_run():
       logger.info('Making test call to /route2')
       r2 = app.test_client().get('http://localhost:5000/route2', headers={ 'tester1': 'tester1', 'tester2':'tester2'})
       # Confirm something was returned.
+      span_list = memoryExporter.get_finished_spans()
       assert span_list
       # Confirm there are three spans
       logger.debug('len(span_list): ' + str(len(span_list)))
@@ -158,20 +151,20 @@ def test_run():
       flaskSpanAsObject = json.loads(span_list[0].to_json())
       # Check that the expected results are in the flask extended span attributes
       assert flaskSpanAsObject['attributes']['http.method'] == 'GET'
-      assert flaskSpanAsObject['attributes']['http.target'] == '/route1'
+      assert flaskSpanAsObject['attributes']['http.target'] == '/route2'
       assert flaskSpanAsObject['attributes']['http.request.header.tester1'] == 'tester1'
       assert flaskSpanAsObject['attributes']['http.request.header.tester2'] == 'tester2'
       assert flaskSpanAsObject['attributes']['http.response.header.content-type'] == 'application/json'
-      assert flaskSpanAsObject['attributes']['http.response.body'] == '{ "a": "a", "xyz": "xyz" }'
+      assert flaskSpanAsObject['attributes']['http.response.body'] == '{"message": "Hello [(\'mysql.infoschema\', \'localhost\')]"}'
       assert flaskSpanAsObject['attributes']['http.status_code'] == 200
       assert flaskSpanAsObject['attributes']['http.response.header.tester3'] == 'tester3'
       memoryExporter.clear()
       logger.info('Reading /route1 response.')
       a1 = r1.get_json()['a']
       logger.info('Reading /route2 response.')
-      a2 = r2.get_json()['a']
+      a2 = r2.get_json()['message']
       assert a1 == 'a'
-      assert a2 == 'a'
+      assert a2 == 'Hello [(\'mysql.infoschema\', \'localhost\')]'
       logger.info('r1 result: ' + str(a1))
       logger.info('r2 result: ' + str(a2))
       logger.info('Exiting from flask instrumentation test.')
