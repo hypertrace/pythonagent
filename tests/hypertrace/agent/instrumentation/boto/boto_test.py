d89e6f270344cc3738dcf42d75c0cf300447c83e
prodion23 <donniepropst@Donnies-MacBook-Pro.local>
2022-03-25 13:45:34 -0400

    add mock to boto test

--- tests/hypertrace/agent/instrumentation/boto/boto_test.py
+++ tests/hypertrace/agent/instrumentation/boto/boto_test.py
@@ -2,16 +2,16 @@ import os
 import sys
 import logging
 import traceback
-import json
-from opentelemetry.sdk.trace.export.in_memory_span_exporter import InMemorySpanExporter
-from opentelemetry.sdk.trace.export import SimpleSpanProcessor
+
+import moto
+
 from hypertrace.agent import Agent
 import boto.awslambda
-
 # @pytest.mark.serial
 from tests import setup_custom_logger, configure_inmemory_span_exporter
 
 
+@moto.mock_lambda()
 def test_run():
     logger = setup_custom_logger(__name__)
     os.environ['_HANDLER'] = 'test_lambda.example_lambda'
@@ -20,26 +20,21 @@ def test_run():
     memory_exporter = configure_inmemory_span_exporter(agent)
 
     logger.info('Running test calls.')
+    lamb = boto.awslambda.connect_to_region("us-east-2")
+
+    # multiple calls
     try:
-        lamb = boto.awslambda.connect_to_region("us-east-2")
-
-        # multiple calls
         lamb.list_functions()
-
-        spans = memory_exporter.get_finished_spans()
-        assert spans
-        assert len(spans) == 1
-        span = spans[0]
-
-        assert span.attributes['endpoint'] == 'lambda'
-        assert span.attributes['http.method'] == 'GET'
-        assert span.attributes['aws.region'] == 'us-east-2'
-        assert span.attributes['aws.operation'] == 'list_functions'
-        memory_exporter.clear()
-
-        return 0
     except:
-        logger.error('Failed to test boto instrumentation wrapper: exception=%s, stacktrace=%s',
-                     sys.exc_info()[0],
-                     traceback.format_exc())
-        raise sys.exc_info()[0]
+        pass
+
+    spans = memory_exporter.get_finished_spans()
+    assert spans
+    assert len(spans) == 1
+    span = spans[0]
+
+    assert span.attributes['endpoint'] == 'lambda'
+    assert span.attributes['http.method'] == 'GET'
+    assert span.attributes['aws.region'] == 'us-east-2'
+    assert span.attributes['aws.operation'] == 'list_functions'
+    memory_exporter.clear()
